2ee09d1feba92eca15f0acda916c6caa5c0d43b8
Prabhat Ranjan <Prabhat0206@hotmail.com>
2022-02-11 13:41:55 +0530

    db

--- API/config.py
+++ API/config.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from os import environ
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = environ.get('DB_URL')
+SQLALCHEMY_DATABASE_URI = environ.get('SQL_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = environ.get('SECRET_KEY')
 ADMIN_EMAIL = environ.get('AMDIN_EMAIL')
