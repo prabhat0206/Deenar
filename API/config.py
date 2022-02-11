61c444c7694d515c82390778856133c17b95c1b2
Prabhat Ranjan <Prabhat0206@hotmail.com>
2022-02-11 15:01:47 +0530

    asdas

--- API/config.py
+++ API/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URI = environ.get('SQL_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = environ.get('SECRET_KEY')
-ADMIN_EMAIL = environ.get('AMDIN_EMAIL')
+ADMIN_EMAIL = environ.get('ADMIN_USERNAME')
 ADMIN_PASSWORD = environ.get('ADMIN_PASSWORD')
 TOKEN = environ.get('TOKEN')
